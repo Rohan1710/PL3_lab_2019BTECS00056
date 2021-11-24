cde315681cce98d9fd29c36fff8dabbd73c95fdb
Rohan <74046613+Rohan1710@users.noreply.github.com>
2021-11-24 11:25:18 +0530

    Create FirstProgram.js

---
+++ Practical_7/FirstProgram.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer(function(req,res){
+    res.write("Hello Node JS");
+    res.end();
+})
+
+server.listen(port,'127.0.0.1');
