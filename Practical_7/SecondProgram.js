208cf5549e021f63a81a7d1d6c9c2d3138d22a47
Rohan <74046613+Rohan1710@users.noreply.github.com>
2021-11-24 11:26:06 +0530

    Create SecondProgram.js

---
+++ Practical_7/SecondProgram.js
@@ -0,0 +1,13 @@
+5+4
+5%2
+5+(5/2-1)
+var name = "Rohan";
+name;
+var rollNo = "2019BTECS00056";
+rollNo
+var x = 0;
+for(let i = 0;i<5;i++)
+{
+    x++;
+    console.log("Current Value of X is " + x);
+}
